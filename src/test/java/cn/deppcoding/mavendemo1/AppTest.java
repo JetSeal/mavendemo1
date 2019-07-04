5530d7d9f8b2b8c67fbf1c193a6284b8c94b271f
JetSeal <jetseal@qq.com>
2019-07-04 15:17:11 +0800

    test1

---
+++ src/test/java/cn/deppcoding/mavendemo1/AppTest.java
@@ -0,0 +1,21 @@
+package cn.deppcoding.mavendemo1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+		//1
+        assertTrue( true );
+    }
+}
